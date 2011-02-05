40ecf8838e5747ef0c6049b34a5109662551e2e4
Morten Nielsen <morten@morkeleb.com>
2011-02-05 23:07:35 +0100

    removing copyright from sample used as baseline

--- chrometorrent.js
+++ chrometorrent.js
@@ -1,8 +1,3 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// A generic onclick callback function.
 
 function ChromeTorrent(host, username, password) {
 	var self = this;
