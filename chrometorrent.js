8faa3bbdab1405e7f303be28908e1b8259b5bf6a
Morten Nielsen <morten@morkeleb.com>
2011-02-05 22:20:14 +0100

    created chrometorrent object with tests

--- chrometorrent.js
+++ chrometorrent.js
@@ -3,17 +3,48 @@
 // found in the LICENSE file.
 
 // A generic onclick callback function.
-var token = "tt";
 
-$.get(localStorage["host"] + "token.html", function(data){
-token = $(data).first().first().html();});
+function ChromeTorrent(host, username, password) {
+	var self = this;
 
-function createTorrentUrl(info) {
-	return localStorage["host"] + "?token=" + token + "&action=add-url&s=" + escape(info.linkUrl);
+	var token = "tt";
+
+	$.ajax({url:host + "token.html", success:function(data) {
+	token = $(data).first().first().html();},
+		username:username, password:password});
+
+	$.get(host + "token.html", function(data){
+	token = $(data).first().first().html();});
+	
+	this.getUser = function() {
+					return username;
+	};
+	this.getHost= function() {
+					return host;
+	};
+	this.getPassword= function() {
+					return password;
+	};
+	this.getToken = function() {
+					return token;
+	};
+	
+	this.createDownloadUrlForTorrent =  function (url) {
+	return self.getHost() + "?token=" + self.getToken() + "&action=add-url&s=" + escape(url);
 }
 
+	this.addTorrent = function(torrentUrl){
+					$.ajax({url:self.createDownloadUrlForTorrent(torrentUrl),
+									username:self.getUser(),
+									password:self.getPassword()});
+	};
+};
+
+
+var myTorrent = new ChromeTorrent(localStorage["host"], localStorage["user"], localStorage["pass"]);
+
 function download(info, tab) {
-  $.get(createTorrentUrl(info));
+  myTorrent.addTorrent(info.linkUrl);
   console.log("item " + info.linkUrl + " was clicked");
 }
 
