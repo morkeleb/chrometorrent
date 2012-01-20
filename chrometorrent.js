f8292d8c27548f61be4bc57dd84d1c702d4c85fb
Peter Hamberg <zazcallabah@gmail.com>
2012-01-20 01:12:13 +0100

    added log that is displayed on option page, couldnt get it to log on ajax error though

--- chrometorrent.js
+++ chrometorrent.js
@@ -13,7 +13,10 @@ function ChromeTorrent() {
 			url:      host + "token.html",
 			success:  function(data) { token = $(data).first().first().html(); if( callback !== undefined ) callback(); },
 			username: username,
-			password: password
+			password: password,
+			error: function( xhr,status,e){
+				throw { message: "when fetching api token. encountered " +status+e};
+			}
 		});
 	};
 	
@@ -31,7 +34,9 @@ function ChromeTorrent() {
 		$.ajax( {
 			url:      self.createDownloadUrlForTorrent(torrent),
 			username: self.getUser(),
-			password: self.getPassword()
+			password: self.getPassword(),
+			error: function(xhr,status,e){ throw {message:"when posting torrent. encountered "+status+e};}
+
 		});
 
 	};
@@ -66,12 +71,59 @@ function ChromeTorrent() {
 	};
 }
 
+function makeLog() {
+	var maxEntries = 100;
+	var loglabel = "logdata";
+	var saveLog = function( log ){
+		localStorage[loglabel] = JSON.stringify(log);
+	};
+
+	var getTime= function(){
+		var t = new Date();
+		return t.toISOString()
+	};
+
+	var ensureNotExceedingMax = function( log ){
+		if( log.length > maxEntries ) {
+			log.splice(0,log.length - maxEntries);
+		}
+		return log;
+	};
+	return {
+		addInfo: function( row ){
+			this.add( getTime() + " INFO: "+row );
+		},
+		addError: function(row){
+			this.add(getTime() + " ERROR: "+row);
+		},
+		add: function( row ) {
+			var log = this.get();
+			log[log.length] = row;
+			saveLog( ensureNotExceedingMax( log ) );
+		},
+		get: function() {
+			var storedlog = localStorage[loglabel];
+			if(storedlog === undefined || storedlog === null )
+				return [];
+			return JSON.parse( storedlog )
+		}
+	};
+};
 
 var myTorrent = new ChromeTorrent();
+var myLog = makeLog();
 
 function download(info, tab) {
+try
+{
   myTorrent.addTorrent(info.linkUrl);
   console.log("item " + info.linkUrl + " was clicked");
+  myLog.addInfo( "clicked: " + info.linkUrl );
+}
+catch(e)
+{
+	myLog.addError( e.message );
+}
 }
 
   var title = "Send to uTorrent";
